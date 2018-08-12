bc99ff6befb47973da879aa8fc85e0a7d3c288a4
qinwang <qinwang.1992@163.com>
2018-08-12 23:55:23 +0800

    rewrite utils

--- dns/Authority/testRrcases.py
+++ dns/Authority/testRrcases.py
@@ -1,46 +1,46 @@
-#coding ==utf-8
+#coding == utf-8
 import pytest
 
-class Rrcases:
+class TestRrcases:
 
-    def addRr(self):
+    def test_addRr(self):
         pass
 
-    def modifyRr(self):
+    def test_modifyRr(self):
         pass
 
-    def deleteRr(self):
-        pass
-
-    @pytest.fixture(scope=function)
-    def deleteAll(self):
-        pass
-
-class shareZoneRr:
-
-    def addRr(self):
-        pass
-
-    def modifyRr(self):
-        pass
-
-    def deleteRr(self):
+    def test_deleteRr(self):
         pass
 
     @pytest.fixture(scope=function)
     def deleteAll(self):
         pass
 
-class shareRr:
+class TestshareZoneRr:
+
+    def test_addRr(self):
+        pass
+
+    def test_modifyRr(self):
+        pass
+
+    def test_deleteRr(self):
+        pass
 
     @pytest.fixture(scope=function)
     def deleteAll(self):
         pass
-    def addRr(self):
+
+class TestshareRr:
+
+    @pytest.fixture(scope=function)
+    def deleteAll(self):
+        pass
+    def test_addRr(self):
         pass
 
-    def modifyRr(self):
+    def test_modifyRr(self):
         pass
 
-    def deleteRr(self):
+    def test_deleteRr(self):
         pass
--- utils/tool.py
+++ utils/tool.py
@@ -1,15 +1,15 @@
 #coding = utf-8
 
-import requests
+from requests import Request
 import json
 import paramiko
 
 class tools:
-    def __init__(self):
+    def __init__(self,func):
         self.host = ''
         self.uri = ''#从接口文档中调用过来的端口+uri
 
-    def send(self,url, methord, param):
+    def __call__(self,url, methord, param):
         print (url)
         requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
         if methord =="post":
@@ -19,7 +19,7 @@ class tools:
             if is_ok == 200:
                 return "pass"
             else:
-                return "flase to run!!!!!!!!"
+                return "fail"
 
         elif methord =="get":
             response = requests.get(url, data = param, verify = False,headers=self.header,auth=("###admin###", "%%%zdns%%%"))
@@ -50,6 +50,54 @@ class tools:
                 return is_ok
 
 
-#远程执行命令
-    def remote(self):
+
+class sendRequest:
+
+    """
+    被修饰函数需要构造出需要的参数param
+    被装饰函数传：
+    “methord”：接口文档获取
+    "uri"：接口文档获取，对于需要在uri中定义视图，区的需要用*标识，以便替换
+    "参数" ：方法自己构造
+
+    """
+
+
+    def __init__(self,func):
+        self._func = func
+        self.header = 'header'
+
+
+    def __call__(self, *args, **kwargs):
+        params = self._func()
+        methord = params['methord']
+        uri = params['uri']
+        param = params['param']
+        response = requests(methord,uri, data = param, verify = False,header = self.header,auth = ("###admin###", "%%%zdns%%%"))
+        return response
+
+    def postRequest(self,*args,**kwargs):
+        uri = kwargs[uri]
+        params = kwargs[params]
+        res = requests.post(uri, data = )
+
+    def putRequest(self,*args,**kwargs):
         pass
+
+    def getRequest(self,*args,**kwargs):
+        pass
+
+    def delRequest(self,*args,**kwargs):
+        pass
+
+
+# #远程执行命令
+#     def remote(self):
+#         pass
+
+
+requests()
+
+
+
+
